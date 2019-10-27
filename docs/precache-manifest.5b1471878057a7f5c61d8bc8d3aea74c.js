d7f3935f1a3e84ff4560647e6e53bf849a02eaab
Ray Tsai <connect@ray-tsai.com>
2019-10-28 09:03:44 +1100

    Add build

---
+++ docs/precache-manifest.5b1471878057a7f5c61d8bc8d3aea74c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f477071c1b43907fcaee5dee39ac5b0",
+    "url": "/ovo-address-book/index.html"
+  },
+  {
+    "revision": "2f7ad0a181ee881d113d",
+    "url": "/ovo-address-book/static/css/main.d6b26b12.chunk.css"
+  },
+  {
+    "revision": "7cee147e472ec8ae3fcf",
+    "url": "/ovo-address-book/static/js/2.c0f8546f.chunk.js"
+  },
+  {
+    "revision": "2f7ad0a181ee881d113d",
+    "url": "/ovo-address-book/static/js/main.2d529009.chunk.js"
+  },
+  {
+    "revision": "31f0cbd141eeb34a66a3",
+    "url": "/ovo-address-book/static/js/runtime-main.87c4c07d.js"
+  },
+  {
+    "revision": "1427b09d394de78df2f0162c591090a8",
+    "url": "/ovo-address-book/static/media/management.1427b09d.svg"
+  }
+]);
